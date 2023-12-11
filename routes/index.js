ad17f153f779ef8e1d345b5358d3122c5448a917
NahuelLu <nahuellucana1999@gmail.com>
2023-12-11 00:35:27 -0300

    Commit this current state of the project that works well, but later I have to implement one more feature about let users be admin to delete any post from page

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
 const PostModel = require("../models/post")
 
 /* GET home page. */
 const get_all_posts_GET = asyncHandler(async (req,res) => {
   const posts = await PostModel.find().populate("author").exec()
-  if(req.user){
-    res.render("index", {
-      title: "Express",
-      userIsLoggedIn: req.isAuthenticated(),
-      membership_status: req.user.membership_status, // Pass the entire request object to the template
-      posts
-    });
-  } else{
-    res.render("index", {
-      title: "Express",
-      userIsLoggedIn: false,
-      membership_status: false, // Pass the entire request object to the template
-      posts
-    });
-  }
+  res.render("index", {
+    title: "Express",
+    userIsLoggedIn: req.isAuthenticated(),
+    membership_status: req.user? req.user.membership_status: false, // is user is logged then need to see his membership_status but if not is logged just return false
+    posts
+  });
 })
 router.get('/', get_all_posts_GET)
 
